dd751e2cb2a7b098420cc4e657f3479c6fe575c5
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2023-02-22 12:32:41 +0300

    saved

--- file_writing.py
+++ file_writing.py
@@ -35,6 +35,5 @@ def writing_scv(info):
 def writing_txt(info):
     file = 'Phonebook.txt'
     with open(file, 'a', encoding='utf-8') as data:
-        data.write(
-            f'Фамилия: {info[0]}\n\nИмя: {info[1]}\n\nНомер телефона: {info[2]}\n\nОписание: {info[3]}\n\n\n')
+        data.write(f'Фамилия: {info[0]}\n\nИмя: {info[1]}\n\nНомер телефона: {info[2]}\n\nОписание: {info[3]}\n\n\n')
 
